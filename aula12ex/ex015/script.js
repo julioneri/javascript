d387fb2f1b4f4bd9de025876a53f98f9a0235283
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-14 18:40:32 -0300

    Finalizando ex015 aula12

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,53 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+    
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique o ano novamente!')
+    } else {
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var gênero = ''
+
+        var img = window.document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'foto-adolescente-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'foto-adolescente-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos.</p>`
+        res.appendChild(img)
+    }
+}
