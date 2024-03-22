8d9146b9269757d509e928aa519927d43ea0f559
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-21 21:09:23 -0300

    Finalizando Projeto 03 de JavaScript

---
+++ projetos/projeto03/main.js
@@ -0,0 +1,45 @@
+// EXERCÍCIO:
+
+// Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+
+// Para salários superiores a R$1.250,00, calcule um aumento de 10%.
+
+// Para inferiores ou iguais, o aumento é de 15%.
+function closeModal() {
+    var resulModal = window.document.querySelector('div#resulmodal')
+    resulModal.style.display = 'none'
+}
+
+function calcular() {
+    var val = window.document.querySelector('input#valor')
+    var resulModal = window.document.querySelector('div#resulmodal')
+    var resultado = window.document.querySelector('p#resul')
+    var iconResul = window.document.querySelector('span#iconResul')
+
+    resulModal.style.display = 'flex'
+
+    if (val.value != '') {
+    
+        var valor = Number(val.value)
+        iconResul.innerHTML = 'currency_exchange'
+
+        if (valor > 1250) {
+            var valorNovo = valor + (valor * 10) / 100
+            resultado.innerHTML = `
+            O salário atual é superior R$1.250,00.<br><br>
+            
+            O funcionário passará a ganhar R$${valorNovo.toFixed(2)}, um aumento de 10% do seu salário atual.`
+        } else {
+            var valorNovo = valor + (valor * 15) / 100
+            resultado.innerHTML = `O salário atual é inferior a R$1.250,00.<br><br>
+            
+            O funcionário passará a ganhar R$${valorNovo.toFixed(2)}, um aumento de 15% do seu salário atual.`
+        }
+
+    } else {
+        iconResul.innerHTML = 'chat_error'
+        resultado.innerHTML = 'Você precisa preencher todos os campos. Por favor, tente novamente!'
+    }
+
+    
+}
