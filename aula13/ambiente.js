563cf138f5a431595afbb98a5fad66b90a052039
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-23 15:26:30 -0300

    Cria ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+
+// Testar antes de fazer
+while (c <= 7) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// Testar depois de fazer
+do {
+    console.log('Do While')
+} while (1 > 2)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
