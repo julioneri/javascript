3f9164c9b373341a3001fe38b58f526f21a7200f
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-26 13:12:49 -0300

    Update do ex016 de JavaScript

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,42 +4,57 @@ function contar() {
     var stepNum = window.document.querySelector('input#stepNum')
     
     var resultado = window.document.getElementById('resul')
+    var total = window.document.getElementById('total')
+    var cont = 0
 
     var start = Number(startNum.value)
     var end = Number(endNum.value)
     var step = Number(stepNum.value)
+    
 
-    // Testando se os valores de início (startNum) e fim (endNum) foram obrigatóriamente preenchidos
-    if (start != '' && end != '' && start > 0 && end > 0) {
-        resultado.textContent = ''
-
-        // Testando se é uma contagem progressiva
+    // Testando se PELO MENOS UM dos valores de início e fim foram obrigatóriamente preenchidos.
+    if (start != '' || end != '') {
+        // Testando se será uma contagem progressiva
         if (start < end) {
-            if (step == '' || step > end || step <= 0) {
-                alert('Na ausência de passos válidos, será considerado 1.')
-                var step = 1
+            // Verificando necessidade de correção do valor para passos.
+            if (step <= 0) {
+                alert('Passo inválido! Considerando passo como 1.')
+                step = 1
                 stepNum.value = 1
             }
-    
-            for (c=start;c<=end;c+=step) {
-                resultado.textContent += `${c} > `
+
+            resultado.textContent = ''
+            for (s=start;s<=end;s+=step) {
+                resultado.textContent += `${s} > `
+                cont++
             }
-    
-        // Testando se é uma contagem regressiva
+            resultado.textContent += 'FIM!'
+            total.innerHTML = `Total de passos: ${cont}`
+
+        // Testando se será uma contagem regressiva
         } else if (start > end) {
-            if (step == '' || step > end || step >= 0) {
-                alert('Na ausência de passos válidos, será considerado -1.')
-                var step = -1
+            // Verificando necessidade de correção do valor para passos.
+            if (step >= 0) {
+                alert('Passo inválido! Considerando passo como -1.')
+                step = -1
                 stepNum.value = -1
             }
-
-            for (c=start;c>=end;c+=step) {
-                resultado.textContent += `${c} > `
+            resultado.textContent = ''
+            for (s=start;s>=end;s+=step) {
+                resultado.textContent += `${s} > `
+                cont++
             }
-        }
-        resultado.textContent += 'FIM'
+            resultado.textContent += 'FIM!'
+            total.innerHTML = `Total de passos: ${cont}`
 
+        // Caso os valores digitados sejam iguais
+        } else {
+            resultado.textContent = 'Você precisa digitar valores diferentes para fazer uma contagem.'
+            total.innerHTML = ''
+            
+        }
     } else {
-        resultado.textContent = 'Por favor, preencha todos os campos com valores positivos e válidos.'
+        resultado.textContent = 'Por favor, preencha todos os campos com valores válidos.'
+        total.innerHTML = ''
     }
 }
