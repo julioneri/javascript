e68721e7ee5ad1692245e047c9402a7c1cee7119
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-26 14:36:23 -0300

    Update do ex017 de JavaScript

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function gerarTabuada() {
+    var num = window.document.getElementById('numero')
+    var numero = Number(num.value)
+
+    var tabuada = window.document.getElementById('tabuada')
+    tabuada.innerHTML = '<option value="0">Digite um número acima</option>'
+
+    if (numero != '') {
+        tabuada.innerHTML = ''
+        
+        for (c=1;c<=10;c+=1) {
+            var opt = window.document.createElement('option')
+            opt.setAttribute('value', tabuada.children.length)
+    
+            opt.innerHTML = `${numero} X ${c} = ${numero*c}`
+            tabuada.appendChild(opt)
+        }
+    }
+}
