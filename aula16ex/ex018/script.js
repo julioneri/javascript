cc7e8968896ed2a9e27b4aadffa67d8bb19aa460
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-03-27 19:44:44 -0300

    Update do ex018 de JavaScript

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,57 +1,60 @@
-let lista = []
+let num = window.document.querySelector('input#txtn')
+let lista = window.document.querySelector('select#listArea')
+let resul = window.document.querySelector('p#resultado')
+let vetores = []
+
+function isNumber(n) {
+    if (Number(n) > 0 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionarValor() {
-    let txtn = window.document.querySelector('input#txtn')
-
-    if (txtn.value.length == 0 || txtn.value < 0 || txtn.value > 100 || procurarNum(txtn.value) == -1) {
-        alert('Valor inválido ou já encontrado na lista.')
-    } else {
-        resultado.innerHTML = ''
-        let num = Number(txtn.value)
-
-        let listArea = window.document.getElementById('listArea')
+    if (isNumber(num.value) && !inList(num.value, vetores)) {
+        resul.innerHTML = ''
+        vetores.push(Number(num.value))
         let item = window.document.createElement('option')
-        item.text = `Valor ${num} adicionado.`
-        item.value = `item${num}`
-        listArea.appendChild(item)
-
-        lista.push(num)
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
-}
-
-function procurarNum(numero) {
-    for (let num in lista) {
-        if (numero == lista[num]) {
-            return -1
-        }
-    }
-}
-
-function somarValores() {
-    let soma = 0
-    for (let num in lista) {
-        soma += lista[num]
-    }
-    return soma
+    num.value = ''
+    num.focus()
 }
 
 function finalizar() {
-    if (lista.length == 0) {
-        alert('Adicione valores antes de finalizar!')
-    } else {
-        let resultado = window.document.getElementById('resultado')
+    if (vetores.length != 0) {
+        resul.innerHTML = ''
 
-        lista.sort()
-        let maiorV = lista[(lista.length)-1]
-        let menorV = lista[0]
-        let totalSoma = somarValores()
-        let mediaV = totalSoma / lista.length
-    
-        resultado.innerHTML =
-        `Ao todo, temos ${lista.length} números cadastrados.<br>
-        O maior valor informado foi ${maiorV}.<br>
-        O menor valor informado foi ${menorV}.<br>
-        Somando todos os valores, temos ${totalSoma}.<br>
-        A média dos valores digitados é ${mediaV.toFixed(2)}.<br>`
+        let tot = vetores.length
+        let maior = vetores[0]
+        let menor = vetores[0]
+        let soma = 0
+        for (pos in vetores) {
+            soma += vetores[pos]
+            if (vetores[pos] > maior)
+                maior = vetores[pos]
+            if (vetores[pos] < menor)
+                menor = vetores[pos]
+        }
+        let media = soma / tot
+        resul.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        resul.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        resul.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        resul.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+        resul.innerHTML += `<p>A média dos valores digitados é ${media.toFixed(2)}</p>`
+    } else {
+        window.alert('Você precisa inserir valores antes de finalizar.')
     }
 }
