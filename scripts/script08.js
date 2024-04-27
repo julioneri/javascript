983d3d11dabac7e165c1aa1b70aba19be653f5c7
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-04-27 12:34:50 -0300

    Create script08.js

---
+++ scripts/script08.js
@@ -0,0 +1,15 @@
+/*
+# Desafio: Criando um Sistema de Gerenciamento de Notas Escolares
+
+Sistema de gerenciamento de notas escolares.
+
+Cada aluno tem as seguintes propriedades:
+1. Nome
+2. Notas (uma lista/array de notas)
+
+O programa é capaz de realizar as seguintes operações:
+1. Adicionar um novo aluno ao sistema com suas respectivas notas.
+2. Listar todos os alunos cadastrados com suas respectivas médias de notas.
+3. Buscar um aluno pelo nome e exibir suas notas e média.
+4. Calcular e exibir a média geral de todas as notas cadastradas.
+*/
